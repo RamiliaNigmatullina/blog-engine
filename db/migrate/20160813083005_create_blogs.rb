db16eba77cffecd3914ae28580ffe2a41f048f6c
Ramilia Nigmatullina <RamiliaNigmatullina@gmail.com>
2016-08-13 11:49:35 +0300

    page posts corrected

--- db/migrate/20160813083005_create_blogs.rb
+++ db/migrate/20160813083005_create_blogs.rb
@@ -1,7 +1,8 @@
 class CreateBlogs < ActiveRecord::Migration
   def change
     create_table :blogs do |t|
-      t.references :blog, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+
       t.timestamps null: false
     end
   end

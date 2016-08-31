2b494df72b3cf5290c96d88007718d13712ede97
Ramilia Nigmatullina <RamiliaNigmatullina@gmail.com>
2016-08-31 22:08:39 +0300

    profile changed

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,7 +1,15 @@
 class UserDecorator < ApplicationDecorator
-  delegate :id, :full_name, :email, :subscriptions, :posts, :blogs, :subscribed_blogs
+  delegate :id, :full_name, :username, :email, :subscriptions, :posts, :blogs, :subscribed_blogs
 
   def full_name_with_email
     "#{object.full_name} (#{object.email})"
+  end
+
+  def posts_number
+    object.posts.count
+  end
+
+  def comments_number
+    object.comments.count
   end
 end

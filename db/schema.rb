324c1358e357eb070a1a1ca397793403e2613b36
Ramilia Nigmatullina <RamiliaNigmatullina@gmail.com>
2016-09-07 13:48:01 +0300

    migrations changed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907122519) do
+ActiveRecord::Schema.define(version: 20160907122520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,11 +75,9 @@ ActiveRecord::Schema.define(version: 20160907122519) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "blog_id"
     t.integer  "user_id"
   end
 
-  add_index "posts", ["blog_id"], name: "index_posts_on_blog_id", using: :btree
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "subscriptions", force: :cascade do |t|

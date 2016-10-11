03b34dd235aa06ba661e0692ba7d3ae353617855
Ramilia Nigmatullina <RamiliaNigmatullina@gmail.com>
2016-10-11 21:12:25 +0300

    model tests added

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,4 +1,12 @@
 require "rails_helper"
 
 describe Like do
+  let!(:user) { create(:user) }
+  let!(:post) { create(:post) }
+  let!(:first_like) { create(:like, user: user, post: post) }
+  let!(:second_like) { build(:like, user: user, post: post) }
+
+  it "does not allow duplicate likes per post" do
+    expect(second_like).not_to be_valid
+  end
 end
